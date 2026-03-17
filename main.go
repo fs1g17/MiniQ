f3467e97e1a5f4b14b45169932938053c4da5fef
fs1g17 <fs1g17@soton.ac.uk>
2026-03-17 14:30:52 +0400

    feat: in-memory queue

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fs1g17/MiniQ/queue"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	q := queue.Queue{}
+
+	job1 := queue.Job{Name: "job1"}
+	job2 := queue.Job{Name: "job2"}
+
+	q.Enqueue(&job1)
+	q.Enqueue(&job2)
+
+	dq1 := q.Dequeue()
+	fmt.Println(dq1.Name)
+
+	dq2 := q.Dequeue()
+	fmt.Println(dq2.Name)
 }
---
+++ queue/queue.go
@@ -0,0 +1,20 @@
+package queue
+
+type Job struct {
+	Name string
+}
+
+type Queue struct {
+	Jobs []*Job
+}
+
+func (q *Queue) Enqueue(job *Job) {
+	q.Jobs = append(q.Jobs, job)
+}
+
+func (q *Queue) Dequeue() *Job {
+	job := q.Jobs[0]
+	q.Jobs = q.Jobs[1:]
+
+	return job
+}
