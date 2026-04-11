e3fcf8951c23213f8c865a5ef57bfd1ee83f61da
fs1g17 <fs1g17@soton.ac.uk>
2026-04-11 17:30:15 +0400

    test(queue): add a job store mock

--- internal/queue/mini_q_test.go
+++ internal/queue/mini_q_test.go
@@ -1,1 +1,36 @@
 package queue
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/fs1g17/MiniQ/internal/store"
+)
+
+type mockJobStore struct {
+	jobs   []*store.Job
+	nextID int
+}
+
+func (m *mockJobStore) GetQueuedJobs() ([]*store.Job, error) {
+	jobs := make([]*store.Job, 0)
+	return jobs, nil
+}
+
+func (m *mockJobStore) InsertJob(job *store.Job) error {
+	job.ID = m.nextID
+	m.nextID++
+	m.jobs = append(m.jobs, job)
+	return nil
+}
+
+func (m *mockJobStore) UpdateJobStatus(jobId int, jobStatus store.JobStatus) error {
+	idx := slices.IndexFunc(m.jobs, func(j *store.Job) bool { return j.ID == jobId })
+	if idx == -1 {
+		return fmt.Errorf("job with id %d not found", jobId)
+	}
+
+	m.jobs[idx].Status = jobStatus
+
+	return nil
+}
